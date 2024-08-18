d82d92621351fe3293dd92efdd94d1d73d07bed0
LuneVoilee <mwz791867916@163.com>
2024-08-18 19:20:35 +0800

    改善静态反射框架

---
+++ SimpleReflection/Traits/FunctionTraits.h
@@ -0,0 +1,55 @@
+﻿// ReSharper disable CppFunctionIsNotImplemented
+#pragma once
+#include <tuple>
+
+// 普通函数 类函数 普通函数指针 类函数指针
+// returnType FunctionType Params
+
+namespace internal
+{
+    template <typename T>
+    struct BaseFunctionTraits;
+
+    template <typename R, typename... Args>
+    struct BaseFunctionTraits<R(Args...)>
+    {
+        using returnType = R;
+        using FunctionType = R(Args...);
+        using Params = std::tuple<Args...>;
+    };
+
+    template <typename T>
+    struct FunctionTraits;
+
+    template <typename R, typename... Args>
+    struct FunctionTraits<R(Args...)>
+        : BaseFunctionTraits<R(Args...)>
+    {
+        using pointerType = R (*)(Args...);
+        static constexpr bool isConst = false;
+        static constexpr bool isMember = false;
+    };
+
+    template <typename R, typename C, typename... Args>
+    struct FunctionTraits<R (C::*)(Args...)>
+        : BaseFunctionTraits<R(Args...)>
+    {
+        using classType = C;
+        using pointerType = R (C::*)(Args...);
+        using argsWithClass = std::tuple<C *, Args...>;
+        static constexpr bool isConst = false;
+        static constexpr bool isMember = true;
+    };
+
+    template <typename R, typename C, typename... Args>
+    struct FunctionTraits<R (C::*)(Args...) const>
+        : BaseFunctionTraits<R(Args...)>
+    {
+        using classType = C;
+        using pointerType = R (C::*)(Args...);
+        using argsWithClass = std::tuple<C *, Args...>;
+        static constexpr bool isConst = true;
+        static constexpr bool isMember = true;
+    };
+};
+
---
+++ SimpleReflection/Traits/VariableTraits.h
@@ -0,0 +1,36 @@
+﻿// ReSharper disable CppFunctionIsNotImplemented
+#pragma once
+
+
+namespace internal {
+    //内置，指针，类指针
+    //type pointertype classtype
+template<typename T>
+struct VariableType{
+    using type = T;
+};
+
+template<typename C,typename T>
+struct VariableType<T C::*>{
+    using type = T;
+};
+
+template<typename T>
+struct VariableTraits;
+
+template<typename T >
+struct VariableTraits<T*>{
+    using type = VariableType<T>::type;
+    using PointerType = T*;
+    static constexpr bool is_member = std::is_member_pointer_v<T>;
+};
+
+template<typename C,typename T >
+struct VariableTraits<T C::*>{
+    using type = VariableType<T>::type;
+    using pointerType = T C::*;
+    using class_t = C;
+    static constexpr bool is_member = std::is_member_pointer_v<T>;
+};
+
+};
